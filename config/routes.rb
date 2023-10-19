2782fcae89e3f688aed61bf8921d70f28f97e7d3
rob.foggo <robfoggo@gmail.com>
2023-10-19 19:27:48 -0400

    CRUD REST APIs updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,28 @@
 Rails.application.routes.draw do
-  get 'main/index'
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/edit'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/new'
+  # get 'categories/edit'
+  # get 'categories/delete'
+  # get 'main/index'
+  resources :categories do
+    member do
+      get :delete
+    end
+  end
+  
+  resources :tasks do
+    member do
+      get :delete
+    end
+  end
+  
+  # get 'tasks/index'
+  # get 'tasks/new'
+  # get 'tasks/edit'
+  # get 'tasks/show'
+  # get 'tasks/delete'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "main#index"
